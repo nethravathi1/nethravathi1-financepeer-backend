1335c26cf074c3cbe396dc9b621bf057b9b8c3f6
nethra <nethravathib04@gmail.com>
2021-12-09 06:46:07 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,155 @@
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3").verbose();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const dbPath = path.join(__dirname, "./financePeer.db");
+const app = express();
+
+app.use(express.json());
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({ filename: dbPath, driver: sqlite3.Database });
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(-1);
+  }
+};
+initializeDBAndServer();
+
+const logger = (request, response, next) => {
+  console.log(request.query);
+  next();
+};
+
+//User Register API
+app.post("/users/", async (request, response) => {
+  const { username, name, password, gender, location } = request.body;
+  const hashedPassword = await bcrypt.hash(request.body.password, 10);
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    const createUserQuery = `
+      INSERT INTO 
+        user (username, name, password, gender, location) 
+      VALUES 
+        (
+          '${username}', 
+          '${name}',
+          '${hashedPassword}', 
+          '${gender}',
+          '${location}'
+        )`;
+    await db.run(createUserQuery);
+    response.send(`User created successfully`);
+  } else {
+    response.status(400);
+    response.send("User already exists");
+  }
+});
+const authenticateToken = (request, response, next) => {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401);
+    response.send("Invalid JWT Token");
+  } else {
+    jwt.verify(jwtToken, "MY_SECRET_TOKEN", async (error, payload) => {
+      if (error) {
+        response.status(401);
+        response.send("Invalid JWT Token");
+      } else {
+        next();
+      }
+    });
+  }
+};
+//User Login API
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+      const payload = {
+        username: username,
+      };
+      const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
+      response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
+  }
+});
+
+//Get Customer API
+app.get("/customer/", authenticateToken, (request, response) => {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401);
+    response.send("Invalid Access Token");
+  } else {
+    jwt.verify(jwtToken, "MY_SECRET_TOKEN", async (error, payload) => {
+      if (error) {
+        response.send("Invalid Access Token");
+      } else {
+        const getcustomerQuery = `
+            SELECT
+                *
+            FROM
+            Customer
+            ORDER BY
+            title;`;
+        const customerArray = await db.all(getcustomerQuery);
+        response.send(booksArray);
+      }
+    });
+  }
+  console.log("hello");
+});
+
+// data stored
+app.post("/customer/", async (request, response) => {
+  const customerDetails = request.body;
+
+  const values = customerDetails.map(
+    (eachUser) =>
+      `('${eachUser.user_id}', ${eachUser.id},${eachUser.title},${eachUser.body})`
+  );
+
+  let db = new sqlite3.Database("./financePeer.db");
+
+  db.run("CREATE TABLE langs(user_id integer,id integer,title text,body text)");
+
+  db.close();
+
+  const valuesString = values.join(",");
+
+  const customerQuery = `
+    INSERT INTO
+      customer (user_id,id,title,body)
+    VALUES
+       ${valuesString};`;
+
+  const dbResponse = await db.run(customerQuery);
+  const customerId = dbResponse.lastID;
+  response.send({ customerId: customerId });
+});
